360b21c724204cd8f17e5ccf8237234ba2334c81
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-04-07 14:21:18 +0530

    added tags, similar posts

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -14,6 +15,7 @@ class PublishedManager(models.Manager):
 class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -43,3 +45,17 @@ class Post(models.Model):
         return self.title
 
     
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+        return 'Comments by {} on {}'.format(self.name, self.post)
